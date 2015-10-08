d20d7ed4557b1f4d3958ddff0a3fa22ac80ab423
Adam Knight <adam@revolutionarysystems.co.uk>
2015-10-08 12:02:27 +0100

    Added logging

--- java-client/src/main/java/uk/co/revsys/account/shield/AccountShieldClientImpl.java
+++ java-client/src/main/java/uk/co/revsys/account/shield/AccountShieldClientImpl.java
@@ -1,9 +1,13 @@
 package uk.co.revsys.account.shield;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.co.revsys.utils.http.BasicAuthCredentials;
 import uk.co.revsys.utils.http.HttpClient;
 import uk.co.revsys.utils.http.HttpClientImpl;
@@ -12,6 +16,8 @@ import uk.co.revsys.utils.http.HttpRequest;
 import uk.co.revsys.utils.http.HttpResponse;
 
 public class AccountShieldClientImpl implements AccountShieldClient {
+
+    final Logger logger = LoggerFactory.getLogger(AccountShieldClientImpl.class);
 
     private HttpClient httpClient;
     private String url;
@@ -46,11 +52,27 @@ public class AccountShieldClientImpl implements AccountShieldClient {
         }
         request.setMethod(HttpMethod.GET);
         Map parameters = new HashMap();
-        parameters.put("case", json.toString());
+        String caseString = json.toString();
+        logger.info(caseString);
+        parameters.put("case", caseString);
         parameters.put("inboundTransformer", "ASLogin.incoming.json");
         parameters.put("processor", "ASLoginProcessor.script");
         request.setParameters(parameters);
         HttpResponse response = httpClient.invoke(request);
+        if (response.getStatusCode() == 200) {
+            InputStream responseStream = response.getInputStream();
+            if (responseStream != null) {
+                logger.info(IOUtils.toString(responseStream));
+                responseStream.close();
+            }
+        } else {
+            logger.error("AS Server returned " + response.getStatusCode());
+            InputStream responseStream = response.getInputStream();
+            if (responseStream != null) {
+                logger.error(IOUtils.toString(responseStream));
+                responseStream.close();
+            }
+        }
         return new CheckLoginResult();
     }
 
